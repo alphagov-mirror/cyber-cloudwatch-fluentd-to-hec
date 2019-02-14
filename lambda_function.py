9b255ae150192d79ad7fef4d6bdce9ef80b7803c
Mohamed Hussein <mohamed.hussein@digital.cabinet-office.gov.uk>
2019-02-14 12:40:29 +0000

    Adding an additional function and modifiying the lambda handler to process HSM logs.
    Pair: @mhussein-gds @akinnane

--- lambda_function.py
+++ lambda_function.py
@@ -19,19 +19,39 @@ def lambda_handler(event, context):
     compressed_data = base64.b64decode(b64encoded_data)
     uncompressed_data = gzip.decompress(compressed_data)
     data = json.loads(uncompressed_data)
-    log_events = data['logEvents']
-    cluster_name = data['logGroup']
-    payload = build_payload(cluster_name, log_events)
+    if 'k8s' in os.environ['SPLUNK_INDEX']:
+        payload = build_payload_k8s(data)
+    send_to_hec(payload)
+    if 'hsm' in os.environ['SPLUNK_INDEX']:
+        payload = build_payload_hsm(data,context)
     send_to_hec(payload)
 
-def build_payload(cluster_name, log_events):
+def build_payload_k8s(data):
     payload = ""
+    log_events = data['logEvents']
+    cluster_name = data['logGroup']
     for log in log_events:
         log = json.loads(log['message'])
         event = {
                     "host": log['kubernetes']['pod_name'],
                     "source": cluster_name,
                     "sourcetype": log['kubernetes']['container_name'],
+                    "index": os.environ['SPLUNK_INDEX'],
+                    "event": log['log']
+                }
+        payload += json.dumps(event)
+    return payload
+
+def build_payload_hsm(data,context):
+    payload = ""
+    log_events = data['logEvents']
+    cluster_name = data['logGroup']
+    for log in log_events:
+        log = json.loads(log['message'])
+        event = {
+                    "host": cluster_name,
+                    "source": context.function_name,
+                    "sourcetype": "cloudhsm",
                     "index": os.environ['SPLUNK_INDEX'],
                     "event": log['log']
                 }
