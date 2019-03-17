01ec5f54dfa106db0399e32be80a05b173ac3276
Alex Kinnane <akinnane@yahoo.co.uk>
2019-03-17 19:57:26 +0000

    Fix imports

--- tests/test_lambda_handler.py
+++ tests/test_lambda_handler.py
@@ -5,5 +5,5 @@ import sys
 currentdir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
+sys.path.insert(0, parentdir + '/fluentdhec')
 import fluentdhec.lambda_function  # noqa
