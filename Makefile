4da7b39738a0315839fed5324c0d6c7e2c48123d
Alex Kinnane <akinnane@yahoo.co.uk>
2019-03-19 12:46:23 +0000

    Add tests

--- Makefile
+++ Makefile
@@ -11,8 +11,7 @@ add_deps: target_dir
 	pip3 install -r requirements.txt --system -t .target
 
 clean:
-	rm -rf .target *.egg-info .tox venv *.zip .pytest_cache
-	find . -type d -regex ".*__pycache__$$" -exec rm -rf {} \;
+	rm -rf .target *.egg-info .tox venv *.zip .pytest_cache htmlcov **/__pycache__
 
 zip: add_deps copy_src
 	cd .target; zip -9 ../cyber-cloudwatch-fluentd-to-hec -r .
--- tests/test_lambda_handler.py
+++ tests/test_lambda_handler.py
@@ -1,9 +1,52 @@
 import os
 import inspect
 import sys
-
+import pytest
+import json
 currentdir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir + '/fluentdhec')
 import fluentdhec.lambda_function  # noqa
+
+
+@pytest.fixture
+def k8s_event():
+    with open('tests/fixtures/cloudwatch_logs_k8s.json') as f:
+        j = json.loads(f.read())
+    return j
+
+
+@pytest.fixture
+def hsm_event():
+    with open('tests/fixtures/cloudwatch_logs_hsm.json') as f:
+        j = json.loads(f.read())
+    return j
+
+
+class Oo():
+    pass
+
+
+@pytest.fixture
+def context():
+    o = Oo()
+    o.function_name = 'function_name'
+    return o
+
+
+def test_lf_send_payload_k8s(k8s_event, context, mocker):
+    mocker.patch.dict(os.environ, {"SPLUNK_INDEX": "k8s"})
+    mocker.patch('fluentdhec.lambda_function.send_to_hec')
+
+    fluentdhec.lambda_function.lambda_handler(k8s_event, context)
+    assert fluentdhec.lambda_function.send_to_hec.call_count == 1
+
+
+def test_lf_send_payload_hsm(hsm_event, context, mocker):
+    mocker.patch.dict(os.environ, {"SPLUNK_INDEX": "hsm"})
+    mocker.patch('fluentdhec.lambda_function.send_to_hec')
+
+    fluentdhec.lambda_function.lambda_handler(hsm_event, context)
+
+    assert fluentdhec.lambda_function.send_to_hec.call_count == 1
